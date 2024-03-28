32f78f5d4748edc77c95839e25f3b7cc3ad96c37
bohdan54844460 <bohdan@horda.dev>
2024-03-28 21:56:38 +0200

    added app classes

---
+++ lib/src/features/app/widgets/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/src/common/util/dependencies.dart';
+
+class App extends StatelessWidget {
+  final InitializationResult result;
+
+  const App({required this.result,super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
